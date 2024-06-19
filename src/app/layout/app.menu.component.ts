544ce873c5669b1f1a78674a4e029363acfe1fb6
raphaelmagalhaesjr <raphaelmagalhaesjr@icloud.com>
2024-06-19 18:45:39 -0300

    add-component2

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -2,35 +2,36 @@ import { OnInit, Component } from '@angular/core';
 import { LayoutService } from './service/app.layout.service';
 
 @Component({
-    selector: 'app-menu',
-    templateUrl: './app.menu.component.html'
+    selector: 'app-menu', // Define o seletor do componente
+    templateUrl: './app.menu.component.html' // Template do componente
 })
 export class AppMenuComponent implements OnInit {
 
-    model: any[] = [];
+    model: any[] = []; // Modelo de dados do menu
 
-    constructor(public layoutService: LayoutService) { }
+    constructor(public layoutService: LayoutService) { } // Injeta o serviço de layout
 
     ngOnInit() {
+        // Inicializa o modelo de dados do menu
         this.model = [
             {
-                label: 'Loja',
-                icon: 'pi pi-fw pi-shop',
+                label: 'Loja', // Rótulo do item de menu principal
+                icon: 'pi pi-fw pi-shop', // Ícone do item de menu principal
                 items: [
                     {
-                        label: 'Cliente',
-                        icon: 'pi pi-fw pi-user',
-                        routerLink: ['/pages/client']
+                        label: 'Cliente', // Rótulo do item de submenu
+                        icon: 'pi pi-fw pi-user', // Ícone do item de submenu
+                        routerLink: ['/pages/client'] // Rota do item de submenu
                     },
                     {
-                        label: 'Pedido',
-                        icon: 'pi pi-fw pi-cart-plus',
-                        routerLink: ['/pages/crud']
+                        label: 'Pedido', // Rótulo do item de submenu
+                        icon: 'pi pi-fw pi-cart-plus', // Ícone do item de submenu
+                        routerLink: ['/pages/crud'] // Rota do item de submenu
                     },
                     {
-                        label: 'Produto',
-                        icon: 'pi pi-fw pi-box',
-                        routerLink: ['/pages/orders']
+                        label: 'Produto', // Rótulo do item de submenu
+                        icon: 'pi pi-fw pi-box', // Ícone do item de submenu
+                        routerLink: ['/pages/orders'] // Rota do item de submenu
                     },
                 ]
             },
